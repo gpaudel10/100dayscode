9187c5d1bf7634037b9f76b9db583e731a99dbff
gpaudel <paudelg09@gmail.com>
2024-04-14 18:27:18 +0545

    first day

---
+++ day1/main.py
@@ -0,0 +1,17 @@
+# print("What to print")
+
+
+# more about input, variable and naming variable
+
+# Band name generaotr
+
+print("Welcome to the band name generator !!")
+
+city = input("Name the city that you grew up?\n")
+
+name_of_pet = input("Name your pet\n")
+
+combine_word = print("Your band name is: " + city + " " + name_of_pet)
+
+print(combine_word)
+
